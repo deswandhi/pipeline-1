d3870ff6f19173a43f337c8185650857bcab801e
deswandhi <deswandhi@gmail.com>
2021-06-19 17:05:56 +0700

    update-2

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+# create list of columns to use
+cols = ['zipcode','agi_stub','mars1','MARS2','NUMDEP' ]
+
+# create data frame from csv using only  selected columns
+data = pd.read_csv("dataset/vt_tax_data_2016.csv", usecols=cols)
+
+# View print of dependents and tax returns by income level-2
+print (data.groupby('agi_stub').sum())
